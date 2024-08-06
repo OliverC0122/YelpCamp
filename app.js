fb2710d33b59a783212f5111f52d992046d0f1f6
Oliver <214707419@qq.com>
2024-08-06 16:22:56 -0400

    added basic CRUD

--- app.js
+++ app.js
@@ -1,15 +1,74 @@
+//importing the required packages
 const express = require('express');
 const path = require("path");
-const app = express()
+const methodOverride = require('method-override');
 
+// setting up the mongo instance
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+
+//setting up express
+const app = express();
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+
+//setting up the middleware.
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+
 const port = 8080;
 
 app.listen(port,()=>{
-    console.log(`listen at port ${port}!`)
-})
+    console.log(`listening at port ${port}!`)
+});
 
 app.get("/",(req,res)=>{
-    res.send("home page");
+    res.render("home");
+});
+
+app.get('/campgrounds', async (req, res) => {
+    //load all the data from the mongo campgrounds collectiton.
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index',{campgrounds});
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);    
+})
+
+
+app.get('/campgrounds/new', async (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show',{campground});
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit',{campground});
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const newCampground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${newCampground._id}`);
+})
+
+app.delete('/campgrounds/:id',async (req,res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');    
 })
---
+++ seeds/index.js
@@ -0,0 +1,35 @@
+// setting up the mongo instance
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+// make a random title name for the campground from tha hard coded data
+const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+// make some random hard coded value for the db.
+const cities = require('./cities');
+const {places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+// seed the db with somehow random data 0.0 .
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50;i++){
+        const random1000 = Math.floor(Math.random() *1000);
+        const {city, state} = cities[random1000];
+        const camp = new Campground({
+            location: `${city}, ${state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        });
+        await camp.save();
+    }
+}
+
+// close the db connection after save the data.
+seedDB().then(() => { 
+    mongoose.connection.close();
+});
